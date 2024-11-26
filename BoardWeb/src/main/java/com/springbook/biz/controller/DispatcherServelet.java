480ce7d82a2175a11a938aaf741f2f94cb3fc935
joshbae119 <mclceo119@gmail.com>
2024-11-26 12:54:11 +0900

    dispatcher 와꾸확인

--- BoardWeb/src/main/java/com/springbook/biz/controller/DispatcherServelet.java
+++ BoardWeb/src/main/java/com/springbook/biz/controller/DispatcherServelet.java
@@ -8,20 +8,36 @@ import javax.servlet.http.HttpServletResponse;
 
 /*** Servlet implementation class DispatcherServelet */
 public class DispatcherServelet extends HttpServlet {
-	
 	private static final long serialVersionUID = 1L;
        
     /*** @see HttpServlet#HttpServlet()     */
     public DispatcherServelet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	
     /**@see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(path);
+		
+		if(path.equals("/login.do")) {
+			System.out.println("로그인 처리");
+		}else if (path.equals("/logout.do")) {
+			System.out.println("로그아웃 처리");
+		}else if (path.equals("/insertBoard.do")) {
+			System.out.println("글등록 처리");
+		}else if (path.equals("/updateBoard.do")) {
+			System.out.println("글 수정 처리");
+		}else if (path.equals("/deleteBoard.do")) {
+			System.out.println("글 삭제 처리");
+		}else if (path.equals("/getBoard.do")) {
+			System.out.println("글 상세 보기 처리");
+		}else if (path.equals("/getBoardList.do")) {
+			System.out.println("글 목록 보기 처리");
+		}
+
 	}
 
 	/*** @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)	 */
